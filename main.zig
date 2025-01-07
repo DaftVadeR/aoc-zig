a901ea27862552232739da48b3047cf87073e989
Ross <tsuyoizuse@gmail.com>
2025-01-07 19:35:12 +0200

    memory errors gone, still need to find out why i cant deinit tho

--- main.zig
+++ main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn main() void {
+pub fn main() !void {
     const file = std.fs.cwd().openFile("./aoc-1-input", .{}) catch |err| {
         std.log.err("Failed to open file: {s}", .{@errorName(err)});
         return;
@@ -8,32 +8,78 @@ pub fn main() void {
 
     defer file.close();
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = true }){};
-    const allocator = gpa.allocator();
+    // var gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = true }){};
+    // const allocator = gpa.allocator();
+    //
+    // defer if (gpa.deinit() == .leak) {
+    //     std.log.err("Memory leak", .{});
+    // };
 
-    defer if (gpa.deinit() == .leak) {
-        std.log.err("Memory leak", .{});
-    };
+    var lineAllocatorI = std.heap.GeneralPurposeAllocator(.{}){};
+    var listAllocatorI = std.heap.GeneralPurposeAllocator(.{}){};
 
-    while (file.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', std.math.maxInt(usize)) catch |err| {
+    const lineAllocator = lineAllocatorI.allocator();
+    const listAllocator = listAllocatorI.allocator();
+
+    // defer {
+    //     const deinit_status = lineAllocatorI.deinit();
+    //     const deinit_status2 = listAllocatorI.deinit();
+    //
+    //     //fail test; can't try in defer as defer is executed after we return
+    //     if (deinit_status == .leak or deinit_status2 == .leak) {
+    //         std.log.err("Memory leak", .{});
+    //     }
+    // }
+
+    // const bytes = try allocator.alloc(u8, 100);
+    // defer allocator.free(bytes);
+
+    // const test_allocator = std.testing.allocator;
+
+    var first_all = std.ArrayList([]const u8).init(listAllocator);
+    var second_all = std.ArrayList([]const u8).init(listAllocator);
+
+    defer {
+        // second_all.deinit();
+        // first_all.deinit();
+
+        // listAllocatorI.deinit();
+        // lineAllocatorI.allocator().free();
+        // lineAllocatorI.deinit();
+        // listAllocator.deinit();
+
+        // listAllocator.free(second_all);
+        // listAllocator.free(first_all);
+    }
+
+    while (file.reader().readUntilDelimiterOrEofAlloc(lineAllocator, '\n', std.math.maxInt(usize)) catch |err| {
         std.log.err("Failed to read line: {s}", .{@errorName(err)});
         return;
     }) |line| {
-        defer allocator.free(line);
+        // defer lineAllocator.free(line);
 
         const delim = "   ";
+
         std.debug.print("delim len {d}", .{delim.len});
 
         var tokenizer = std.mem.tokenize(u8, line, delim);
 
-        const first = tokenizer.next();
-        const second = tokenizer.next();
+        const empty: []u8 = "";
 
-        if (first != null and second != null) {
-            std.debug.print("Found split strings, {s} and {s}!\n", .{ first.?, second.? });
+        const first = tokenizer.next() orelse empty;
+        const second = tokenizer.next() orelse empty;
+
+        if (!std.mem.eql(u8, first, empty) and !std.mem.eql(u8, second, empty)) {
+            try first_all.append(first);
+            try second_all.append(second);
+
+            std.debug.print("Found and appended split strings to lists, {s} and {s}!\n", .{ first, second });
         } else {
             std.debug.print("Error parsing line {s}", .{line});
+            std.debug.panic("Eror parsing line", .{});
         }
     }
-    std.debug.print("Done", .{});
+
+    std.debug.print("List 1 {any}", .{first_all});
+    std.debug.print("List 2 {any}", .{second_all});
 }
